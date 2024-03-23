4efba289c2f4e720dccad46f58bbd4ad961b6272
rinorselmani9 <rinorselmani9@gmail.com>
2024-03-23 11:02:46 +0100

    functionality for modal and installed react-bootstrap

--- src/components/Projects/Card.jsx
+++ src/components/Projects/Card.jsx
@@ -1,15 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
 
-const Card = ({project}) => {
-    console.log({project})
+const Card = ({ project }) => {
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => {
+    setShowModal(true);
+  };
+  const closeModal = () => {
+    setShowModal(false);
+  };
   return (
     <div>
-        <p>image</p>
-        <h5>{project?.preview_heading}</h5>
-        <p>{project?.preview_description}</p>
-        <button>Open Modal</button>
-    </div>
-  )
-}
+      <p>image</p>
+      <h5>{project?.preview_heading}</h5>
+      <p>{project?.preview_description}</p>
+      <button onClick={openModal}>Open Modal</button>
 
-export default Card
+      <Modal show={showModal} onHide={closeModal}>
+        <Modal.Header closeButton></Modal.Header>
+        <Modal.Body>
+          <p>{project?.modal?.first_image}</p>
+          <div>
+            <div>
+              {project?.modal?.right_content?.map((content) => {
+                return (
+                  <div>
+                    <h1>{content?.title}</h1>
+                    <p>{content?.description}</p>
+                  </div>
+                );
+              })}
+            </div>
+            <div>
+              {project?.modal?.left_content?.map((content) => {
+                return (
+                  <div>
+                    <h1>{content?.title}</h1>
+                    <p>{content?.description}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <p>{project?.modal?.second_image}</p>
+          <div>
+            <h1>{project?.modal?.my_solution?.title}</h1>
+            <ol>
+              {project?.modal?.my_solution?.solution_steps.map((solution) => {
+                return (
+                  <li>
+                    {solution?.heading}
+                    <ul>
+                      {solution?.bullet_points.map((point) => {
+                        return <li>{point}</li>;
+                      })}
+                    </ul>
+                  </li>
+                );
+              })}
+            </ol>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/Projects/content.js
+++ src/components/Projects/content.js
@@ -5,28 +5,50 @@ export const projects = [
         preview_description:"ICK training program project",
 
         modal:{
-            first_image:"",
-            second_image:"",
+            first_image:"first_image",
+            second_image:"second_image",
             right_content:[
                 {
-                    title:"",
-                    description:""
+                    title:"Tool",
+                    description:"Figma"
                 },
                 {
-                    title:"",
-                    description:""
+                    title:"Duration",
+                    description:"1 week"
                 },
                 {
-                    title:"",
-                    description:""
+                    title:"Target Audience",
+                    description:"1 week"
                 },
             ],
             left_content:[
                 {
-                    title:"",
-                    description:""
+                    title:"Project Brief",
+                    description:"Redesign the landing page to improve user experience, while maintaining brand identity through the primary color palette."
                 }
-            ]
+            ],
+            my_solution:{
+                title:'My Solution',
+                solution_steps:[
+                    {
+                        heading:'Pain point identification',
+                        bullet_points:[
+                            'Confusing navigation',
+                            'Cluttered messaging',
+                            'Overall design feels outdated compared to current design trends'
+                        ]
+                    },
+                    {
+                        heading:'Pain point identification',
+                        bullet_points:[
+                            'Confusing navigation',
+                            'Cluttered messaging',
+                            'Overall design feels outdated compared to current design trends',
+                            'Redesigned footer for a more modern look'
+                        ]
+                    },
+                ]
+            }
         }
     },
     {
